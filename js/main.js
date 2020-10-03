e721ae9516ec7c0ad50300038b6539478582e430
rachsg7 <rachsg7@gmail.com>
2020-10-02 20:57:37 -0700

    new index

--- js/main.js
+++ js/main.js
@@ -13,10 +13,67 @@ const links = [{
     }
 ]
 
+const links2 = {
+    "Week 1": {
+        links: [{
+                label: "Notes",
+                url: "Week1/notes.html"
+            },
+            {
+                label: "Story Editor",
+                url: "Week1/story_editor.html"
+            }
+        ]
+    },
+    "Week 2": {
+        links: [{
+                label: "Notes",
+                url: "Week2/notes.html"
+            },
+            {
+                label: "Team project",
+                url: "Week2/team.html"
+            }
+        ]
+    },
+    "Week 3": {
+        links: [{
+            label: "Notes",
+            url: "Week3/notes.html"
+        }]
+    }
+
+}
+
 // Populate html with links
-let ol = document.getElementById('block1-list');
+/*let ol = document.getElementById('block1-list');
 for (let i = 0; i < links.length; i++) {
     let li = document.createElement('li');
     li.innerHTML = '<a href=\"' + links[i].url + '\">' + links[i].label + '</a>';
     ol.appendChild(li);
+}*/
+
+// Populate html with links
+
+let week = document.getElementById('block1-list');
+for (var item in links2) {
+    let header = document.createElement('h3');
+    header.innerHTML = item;
+    week.appendChild(header);
+    console.log(item);
+    for (let link in links2[item]) {
+        for (let obj in links2[item][link]) {
+            console.log(links2[item][link][obj]);
+        }
+        //console.log(links2[item][link].length);
+    }
+    //console.log(links2[item]);
 }
+
+
+function openBlockOne() {
+    ol.classList.toggle('hidden');
+}
+
+const block1 = document.getElementById('block1');
+block1.addEventListener('click', openBlockOne);
